a1bf28c5fef60348afd4e11e97c82caecb22372b
Kapilneupane11 <kpilneupane11@gmail.com>
2020-02-28 16:52:53 +0545

    Initial Setup

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+from django.shortcuts import reverse
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length = 50)
+    description = models.CharField(max_length=300)
+
+
+class Items(models.model):
+    title = models.CharField(max_length = 200)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+    def get_absolute_url(self):
+        return reverse("home:products", kwargs={'slug', self.slug})
--- home/views.py
+++ home/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import view as view
+from django.shortcuts import render,redirect,get_object_or_404
+from home.models import Items
 
 # Create your views here.
+
+class BaseView(view):
+    template_context = {
+        'items' : Items.objects.all()
+    }
+class HomeView(BaseView):
+    def __get__(self, request):
+        self.template_context['items'] = Items.objects.all()
+
+        return render(request, 'shop-index.html', self.template_context)
